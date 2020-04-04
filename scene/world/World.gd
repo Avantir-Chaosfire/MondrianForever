5a05bbb2e623b773da65b4901bbb9cf483a5d044
Eddie Marchioni <emarchio@sfu.ca>
2020-04-03 18:43:20 -0700

    Adds level switching hotkey

--- scene/world/World.gd
+++ scene/world/World.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
 var levelClasses = [
-	preload("res://scene/level/1/Level1.tscn")
+	preload("res://scene/level/1/Level1.tscn"),
+	preload("res://scene/level/1/Level1_tutorial.tscn")
 ]
 
 var currentLevel = null
+var currentLevelIndex = 0
 
 func _ready():
-	currentLevel = levelClasses[0].instance()
+	currentLevel = levelClasses[currentLevelIndex].instance()
+	add_child(currentLevel)
+
+func _process(delta):
+	if Input.is_action_just_pressed("next_level"):
+		advanceLevel()
+		
+func advanceLevel():
+	remove_child(currentLevel)
+	currentLevelIndex += 1
+	if currentLevelIndex >= len(levelClasses):
+		currentLevelIndex = 0
+	currentLevel = levelClasses[currentLevelIndex].instance()
 	add_child(currentLevel)
