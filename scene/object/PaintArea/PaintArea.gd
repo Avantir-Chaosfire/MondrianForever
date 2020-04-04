4333bcfa4feacf899d3de544856e9cff63885e25
Eddie Marchioni <emarchio@sfu.ca>
2020-04-03 18:26:56 -0700

    Adds coloured panes

--- scene/object/PaintArea/PaintArea.gd
+++ scene/object/PaintArea/PaintArea.gd
@@ -4,6 +4,8 @@ onready var borderDetector = get_node("BorderDetector")
 
 const rayCastDistance = 4000
 
+var paintColour = "white"
+
 func _ready():
 	var negativeXDistance = getBorderDistance(Vector2(-1, 0))
 	var positiveXDistance = getBorderDistance(Vector2(1, 0))
@@ -23,3 +25,16 @@ func getBorderDistance(direction):
 	if borderDetector.is_colliding():
 		return (position - get_parent().to_local(borderDetector.get_collision_point())).length()
 	return 0
+
+func setPaintColour(newPaintColour):
+	paintColour = newPaintColour
+	if paintColour == "white":
+		modulate = Color(255, 255, 255)
+	elif paintColour == "black":
+		modulate = Color(0, 0, 0)
+	elif paintColour == "red":
+		modulate = Color(255, 0, 0)
+	elif paintColour == "blue":
+		modulate = Color(0, 0, 255)
+	elif paintColour == "yellow":
+		modulate = Color(255, 255, 0)
---
+++ scene/object/YellowPaintArea/YellowPaintArea.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var paintAreaClass = preload("res://scene/object/PaintArea/PaintArea.tscn")
+
+func _process(delta):
+	var paintArea = paintAreaClass.instance()
+	paintArea.setPaintColour("yellow")
+	paintArea.position = position
+	get_parent().add_child(paintArea)
+	queue_free()
